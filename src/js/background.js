24013196b251a636d061675c61e8d9204781b042
yaozeyuan <yaozeyuan001@lianjia.com>
2018-11-12 22:29:59 +0800

    feat: 测试代码

--- src/js/background.js
+++ src/js/background.js
@@ -1,28 +1,27 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+ 
+let accessControlRequestHeaders;
+let exposedHeaders;
 
-var accessControlRequestHeaders;
-var exposedHeaders;
+const REQUEST_HEADER_Origin = 'Origin'
+const REQUEST_HEADER_access_control_request_headers = 'Origin'
 
-var requestListener = function(details){
-	var flag = false,
-		rule = {
-			name: "Origin",
-			value: "http://evil.com/"
-		};
-	var i;
+const RESPONSE_HEADER_Access_Control_Allow_Origin = 'Access-Control-Allow-Origin'
 
-	for (i = 0; i < details.requestHeaders.length; ++i) {
-		if (details.requestHeaders[i].name.toLowerCase() === rule.name.toLowerCase()) {
-			flag = true;
-			details.requestHeaders[i].value = rule.value;
+let requestListener = function(details){
+	let currentHost = '' // @todo 待替换成发起请求的地址
+
+	for (let i = 0; i < details.requestHeaders.length; ++i) {
+		if (details.requestHeaders[i].name.toLowerCase() === REQUEST_HEADER_Origin.toLowerCase()) {
+			details.requestHeaders[i].value = currentHost;
 			break;
 		}
 	}
-	if(!flag) details.requestHeaders.push(rule);
+	details.requestHeaders.push(rule);
 	
-	for (i = 0; i < details.requestHeaders.length; ++i) {
-		if (details.requestHeaders[i].name.toLowerCase() === "access-control-request-headers") {
+	for (let i = 0; i < details.requestHeaders.length; ++i) {
+		if (details.requestHeaders[i].name.toLowerCase() === REQUEST_HEADER_access_control_request_headers) {
 			accessControlRequestHeaders = details.requestHeaders[i].value	
 		}
 	}	
@@ -30,26 +29,19 @@ var requestListener = function(details){
 	return {requestHeaders: details.requestHeaders};
 };
 
-var responseListener = function(details){
-	var flag = false,
-	rule = {
-			"name": "Access-Control-Allow-Origin",
-			"value": "*"
-		};
+let responseListener = function(details){
+	let originHost = '' // @todo 待替换成cros的地址
 
-	for (var i = 0; i < details.responseHeaders.length; ++i) {
-		if (details.responseHeaders[i].name.toLowerCase() === rule.name.toLowerCase()) {
-			flag = true;
-			details.responseHeaders[i].value = rule.value;
+	for (let i = 0; i < details.responseHeaders.length; ++i) {
+		if (details.responseHeaders[i].name.toLowerCase() === RESPONSE_HEADER_Access_Control_Allow_Origin.toLowerCase()) {
+			details.responseHeaders[i].value = originHost;
 			break;
 		}
 	}
-	if(!flag) details.responseHeaders.push(rule);
+	details.responseHeaders.push(rule);
 
 	if (accessControlRequestHeaders) {
-
 		details.responseHeaders.push({"name": "Access-Control-Allow-Headers", "value": accessControlRequestHeaders});
-
 	}
 
 	if(exposedHeaders) {
@@ -64,38 +56,38 @@ var responseListener = function(details){
 
 /*On install*/
 chrome.runtime.onInstalled.addListener(function(){
-	chrome.storage.local.set({'active': false});
-	chrome.storage.local.set({'urls': ["<all_urls>"]});
-	chrome.storage.local.set({'exposedHeaders': ''});
-	reload();
+	// chrome.storage.local.set({'active': false});
+	// chrome.storage.local.set({'urls': ["<all_urls>"]});
+	// chrome.storage.local.set({'exposedHeaders': ''});
+	// reload();
 });
 
 /*Reload settings*/
 function reload() {
-	chrome.storage.local.get({'active': false, 'urls': ["<all_urls>"], 'exposedHeaders': ''}, function(result) {
+	// chrome.storage.local.get({'active': false, 'urls': ["<all_urls>"], 'exposedHeaders': ''}, function(result) {
 
-		exposedHeaders = result.exposedHeaders;
+	// 	exposedHeaders = result.exposedHeaders;
 
-		/*Remove Listeners*/
-		chrome.webRequest.onHeadersReceived.removeListener(responseListener);
-		chrome.webRequest.onBeforeSendHeaders.removeListener(requestListener);
+	// 	/*Remove Listeners*/
+	// 	chrome.webRequest.onHeadersReceived.removeListener(responseListener);
+	// 	chrome.webRequest.onBeforeSendHeaders.removeListener(requestListener);
 
-		if(result.active) {
-			chrome.browserAction.setIcon({path: "on.png"});
+	// 	if(result.active) {
+	// 		chrome.browserAction.setIcon({path: "on.png"});
 
-			if(result.urls.length) {
+	// 		if(result.urls.length) {
 
-				/*Add Listeners*/
-				chrome.webRequest.onHeadersReceived.addListener(responseListener, {
-					urls: result.urls
-				},["blocking", "responseHeaders"]);
+	// 			/*Add Listeners*/
+	// 			chrome.webRequest.onHeadersReceived.addListener(responseListener, {
+	// 				urls: result.urls
+	// 			},["blocking", "responseHeaders"]);
 
-				chrome.webRequest.onBeforeSendHeaders.addListener(requestListener, {
-					urls: result.urls
-				},["blocking", "requestHeaders"]);
-			}
-		} else {
-			chrome.browserAction.setIcon({path: "off.png"});
-		}
-	});
+	// 			chrome.webRequest.onBeforeSendHeaders.addListener(requestListener, {
+	// 				urls: result.urls
+	// 			},["blocking", "requestHeaders"]);
+	// 		}
+	// 	} else {
+	// 		chrome.browserAction.setIcon({path: "off.png"});
+	// 	}
+	// });
 }
